86e55f35d5ebb06a35646aabb9b5017f84a2d8d6
teghitsugaya <44857533+teghitsugaya@users.noreply.github.com>
2022-10-10 23:10:40 +0700

    Create dcoker_install.sh

---
+++ dcoker_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common 
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+
+sudo apt-get update 
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+systemctl enable docker
+# add current user to docker group so there is no need to use sudo when running docker
+sudo usermod -aG docker $(whoami)
+sudo chmod 666 /var/run/docker.sock
